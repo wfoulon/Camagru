f528f8564787edc24f0f9bb8786ff3c951a892d9
Wilfrid FOULON <wfoulon@e2r7p2.42.fr>
2018-05-16 17:10:09 +0200

    reset_username

--- change_password.php
+++ change_password.php
@@ -9,7 +9,7 @@ include 'script/reset.php'
         <link href=stylesheets/index.css rel="stylesheet" />
         <link rel="icon" type="image/png" href="img/homelogo.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <title>Reset Password</title>
+        <title>Change Password</title>
     </head>
     <body>
         <div class="page-container" align="center">
--- reset_username.php
+++ reset_username.php
@@ -1,3 +1,17 @@
 <?php
 include "script/reset.php";
 ?>
+
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width initial-scale=1.0" />
+        <link href="stylesheets/index.css" rel="stylesheet"/>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+        <link rel="icon" type="image/png" href="img/homelogo.png" />
+        <title>Change Username</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
